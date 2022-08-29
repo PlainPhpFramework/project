d2a1ab1c74c0f95f25fa51f618f47b4f45b27d8c
Francesco Terenzani <f.terenzani@gmail.com>
2022-08-29 15:40:52 +0200

    New release dev

---
+++ app/config/routing.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * URL rewriting rules
+ */
+return (object) [
+	'params' => [
+		'slug' => '[a-z0-9-]+',
+		'id' => '\d+',
+	],
+	'routes' => [
+		// Examples:
+		//'/' => 'homepage/index',
+		'/hello/:slug' => 'default/greet_user',
+		'/controller/:@action' => 'controller/long/name'
+	],
+	'default_controller' => 'default',
+	'default_action' => 'index',
+	'controller_class_pattern' => '\App\Controller\%s_controller',
+];
--- app/lib/Controller/helloworld_controller.php
+++ app/lib/Controller/helloworld_controller.php
@@ -1,7 +1,7 @@
 <?php
 namespace App\Controller;
 
-class helloworld
+class helloworld_controller
 {
 	function index() 
 	{
